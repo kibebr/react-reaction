dd85001f59f2a2816e6bfa93142dd2c543bbee5a
kibebr <0763722@students.dadeschools.net>
2021-04-14 00:13:41 +0100

    finished Username value object

--- api/lib/domain/Username.ts
+++ api/lib/domain/Username.ts
@@ -1,9 +1,24 @@
-import { Either } from 'fp-ts/Either'
+import { left, right, Either } from 'fp-ts/Either'
+import { isAlphanumeric } from '../utils/String'
+import { iso, Newtype } from 'newtype-ts'
+
+export interface Username extends Newtype<{ readonly Username: unique symbol }, string> {}
+
+const usernameIso = iso<Username>()
 
 type DomainError
   = 'UsernameNotAlpha'
   | 'UsernameTooLong'
   | 'UsernameTooShort'
 
-export const validateUsername = (u: string): Either<DomainError, string> => {
+export const parseUsername = (u: string): Either<DomainError, Username> => {
+  if (!isAlphanumeric(u)) {
+    return left('UsernameNotAlpha')
+  } else if (u.length > 15) {
+    return left('UsernameTooLong')
+  } else if (u.length <= 0) {
+    return left('UsernameTooShort')
+  } else {
+    return right(usernameIso.wrap(u))
+  }
 }
