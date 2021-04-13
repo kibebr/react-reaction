b2cd267e0027b836f60e0653d561784ca4dcc374
kibebr <0763722@students.dadeschools.net>
2021-04-13 23:51:19 +0100

    added jest configuration file for the api

---
+++ api/jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  roots: [
+    '<rootDir>/test'
+  ],
+  testMatch: [
+    '**/__tests__/**/*.+(ts|tsx|js)',
+    '**/?(*.)+(test).+(ts|tsx|js)'
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  }
+}
