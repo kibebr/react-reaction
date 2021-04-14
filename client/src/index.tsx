d1e417aef8e1d930a2dffa05df88c7b2ee41849c
kibebr <0763722@students.dadeschools.net>
2021-04-14 02:40:53 +0100

    added scoring section

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -54,13 +54,17 @@ const App = (): JSX.Element => {
 
   return (
     <>
-      <section className='flex flex-col items-center h-full'>
+      <section className={classNames('flex flex-col h-auto', {
+        'bg-green-500': buttonState === 'PRESS',
+        'bg-yellow-500': buttonState === 'WAIT',
+        'bg-blue-500': buttonState === 'PAUSED'
+      })}>
+        <header className='max-w-screen-xl p-4 md:px-0 m-0 m-auto w-full'>
+          <h1 className='text-white font-bold text-3xl md:text-5xl'>Reaction Test</h1>
+        </header>
+
         <button
-          className={classNames('w-full h-3/4 shadow-lg', {
-            'bg-green-500': buttonState === 'PRESS',
-            'bg-yellow-500': buttonState === 'WAIT',
-            'bg-blue-500': buttonState === 'PAUSED'
-          })}
+          className='w-full h-96 shadow-lg'
           onClick={onButtonClick}
         >
           <div className='flex flex-col text-white font-bold text-4xl'>
@@ -76,6 +80,27 @@ const App = (): JSX.Element => {
               }</span>
           </div>
         </button>
+      </section>
+
+      <div className='my-5' />
+
+      <section className='max-w-screen-xl px-4 md:px-0 m-0 m-auto'>
+        <h2 className='text-3xl font-bold'>Your scores</h2>
+
+        <div className='my-3' />
+
+        <div className='m-0 m-auto bg-white p-4 rounded-md shadow-md max-w-screen-lg'>
+          <div className='flex flex-row justify-center space-x-4'>
+            <div className='flex flex-col items-center'>
+              <span className='font-bold text-2xl'>{combo}</span>
+              <span className='font-bold text-gray-600'>COMBO</span>
+            </div>
+            <div className='flex flex-col items-center'>
+              <span className='font-bold text-2xl'>500ms</span>
+              <span className='font-bold text-gray-600'>FASTEST TIME</span>
+            </div>
+          </div>
+        </div>
       </section>
     </>
   )
